5dd9eba0a9c102c23619a3fee81363bb8da02bb7
Tris Forster <tris.git@tfconsulting.com.au>
2012-01-04 15:32:05 +1100

    Fixed PHPSecLib file transfer - not ideal as opens second connection but works as a backup method

--- classes/task/phpseclib.php
+++ classes/task/phpseclib.php
@@ -2,28 +2,48 @@
 
 $phpseclib = realpath(dirname(__FILE__) . "/../../") . "/vendor/phpseclib";
 set_include_path(get_include_path() . PATH_SEPARATOR . $phpseclib);
-require "Net/SSH2.php";
 
 class Task_PHPSecLib extends Task_Cmd {
   const BOUNDARY = '==jv8dvngvn94d=';
 
-  private $ssh;
+  private $ssh, $sftp;
 
+  private $addr, $port, $user, $auth;
+  
   function __construct($minion) {
     parent::__construct($minion);
-    $this->ssh = new Net_SSH2($minion->get('core.ip'), $minion->get('core.port', 22));
-    $auth = $minion->get('core.authkey', false);
-    if ($auth) {
-      // load the key
+    
+    $this->addr = $minion->get('core.ip');
+    $this->port = $minion->get('core.port', 22);
+    $this->user = $minion->get('core.username');
+    $this->auth = $minion->get('core.authkey', false);
+    if ($this->auth) {
+      throw new Task_Exception("Not yet implemented");
     } else {
-      $auth = $minion->get('core.password');
-    }
-    if (!$this->ssh->login($minion->get('core.username'), $auth)) {
-      throw new Task_Exception("Connection to {$minion->get('core', 'ip')} failed");
+      $this->auth = $minion->get('core.password');
     }
   }
-
+  
+  function _ssh() {
+    require "Net/SSH2.php";
+    $ssh = new Net_SSH2($this->addr, $this->port);
+    if (!$ssh->login($this->user, $this->auth)) {
+      throw new Task_Exception("Incorrect username or password for {$this->addr}");
+    }
+    return $ssh;
+  }
+  
+  function _sftp() {
+    require "Net/SFTP.php";
+    $sftp = new Net_SFTP($this->addr, $this->port);
+    if(!$sftp->login($this->user, $this->auth)) {
+      throw new Task_Exception("Incorrect username or password for {$this->addr}");
+    }
+    return $sftp;
+  }
+  
   function exec($cmd, &$output, &$ret) {
+    if(!$this->ssh) $this->ssh = $this->_ssh();
     $ret_cmd = $cmd . '; echo __$?__';
     $output = explode("\n", trim($this->ssh->exec($ret_cmd)));
     $result = array_pop($output);
@@ -40,25 +60,20 @@ class Task_PHPSecLib extends Task_Cmd {
    * @param type $dest
    * @param type $elevate
    */
-  function copy_to($source, $dest, $elevate=false) {
+  function copy_to($local, $remote, $elevate=false) {
+    if(!$this->sftp) $this->sftp = $this->_sftp();
     if ($elevate) {
       throw new Task_Exception("Not yet implemented");
     } else {
-      $cmd = "cat > {$dest}";
+      $dest = $remote;
     }
-    $data = file_get_contents($source);
-
-    // set a boundary marker
-    $cmd .= '; echo ' . self::BOUNDARY;
-
-    $this->ssh->write($cmd . "\n");
-    $this->ssh->write($data . "\n" . chr(4));
-    // need to read twice
-    $this->ssh->read(self::BOUNDARY);
-    echo $this->ssh->read(self::BOUNDARY);
+    
+    $data = file_get_contents($local);
+    $this->sftp->put($remote, $data);
   }
 
   function copy_from($source, $dest, $elevate=false) {
+    if(!$this->sftp) $this->sftp = $this->_sftp();
     $cmd = "cat \"$source\"";
     if ($elevate)
       $cmd = $this->_elevate($cmd);
