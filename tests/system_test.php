e7815ad35fb0e6dbbfad35ed95a218dfe8a0e893
Tris Forster <tris.git@tfconsulting.com.au>
2012-02-09 10:10:42 +1100

    Completed system tests

--- tests/system_test.php
+++ tests/system_test.php
@@ -11,6 +11,34 @@ class System_Test extends Cmd_Test {
   function testOS() {
     $this->assertEquals('ubuntu', $this->minion->speck('system.os'));
   }
+  
+  function testKernelVersion() {
+    $this->assertEquals('3.0.0', $this->minion->speck('system.kernel_version'));
+  }
+  
+  function testCPUInfo() {
+    $data =<<< EOF
+vendor_id       : GenuineIntel
+cpu cores       : 2
+
+EOF;
+    $this->cmd->data['cat /proc/cpuinfo'] = explode("\n", $data);
+    $this->assertEquals(
+            array(
+                'vendor_id' => 'GenuineIntel',
+                'cpu cores' => '2',
+            ), $this->minion->speck('system.cpuinfo'));
+  }
+  
+  function testHostname() {
+    $this->cmd->data['hostname'] = array('test_system');
+    $this->assertEquals('test_system', $this->minion->speck('system.hostname'));
+  }
+  
+  function testTimeOffset() {
+    $this->cmd->data['date -R'] = array(date('r'));
+    $this->assertEquals(0, $this->minion->speck('system.time_offset'));
+  }
 
 }
 
