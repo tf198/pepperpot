72ce15a6b383996da333c4e6758a7bb8a86b5c47
Tris Forster <tris.git@tfconsulting.com.au>
2012-05-23 22:22:06 +1000

    Blank passwords in log output

--- classes/minion.php
+++ classes/minion.php
@@ -47,7 +47,7 @@ class Minion {
   function get($key, $default=null) {
     $value = $this->cache->get($key, $default);
     $filtered = ($key!='config.password') ? $value : '********';
-    $this->log("GET> {$key} = " . var_export($value, true));
+    $this->log("GET> {$key} = " . var_export($filtered, true));
     return $value;
   }
   
