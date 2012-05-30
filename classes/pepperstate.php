e05485e3b8cfa395dde210cc89a7862285ba4139
Tris Forster <tris.git@tfconsulting.com.au>
2012-05-30 11:56:38 +1000

    Tidied up PepperState and documented

--- classes/pepperstate.php
+++ classes/pepperstate.php
@@ -1,21 +1,56 @@
 <?php
+/**
+ * PepperPot makefile processor
+ * Tries to bring a system to a specified state processing dependancies as required
+ * @author Tris Forster
+ * @package PepperPot
+ */
 class PepperState {
 
+	/**
+	 * Internal dependancy tree representation
+	 * @var multitype:string
+	 */
 	private $states;
+	
+	/**
+	 * Reference to current minion
+	 * @var Minion
+	 */
 	private $minion;
 
+	/**
+	 * Track current recusion level
+	 * @var int
+	 */
 	private $level=0;
+	
+	/**
+	 * Pretty print stack
+	 * @var string
+	 */
 	private $indent='';
 
+	/**
+	 * Construct a new PepperState object
+	 * @param Minion $minion
+	 * @param multitype:string $states
+	 */
 	function __construct($minion, $states) {
 		$this->states = $states;
 		$this->minion = $minion;
 	}
 
+	/**
+	 * Run a specific target
+	 * @param string $target			target to run
+	 * @param multitype:bool $run_cache	internal use only
+	 */
 	function run($target, &$run_cache=null) {
 
 		$this->minion->log("{$this->indent}+ {$target}");
 		
+		// return from run cache if possible - allows for complex dependancy trees
 		if(isset($run_cache[$target])) {
 			$this->minion->log("{$this->indent}- {$target} (cached)");
 			return $run_cache[$target];
@@ -34,7 +69,8 @@ class PepperState {
 			foreach($this->states[$key] as $dep) $deps[] = strtr($dep, $tr);
 		}
 		
-		$run_required = false;
+		// base position depends on whether there is a current persistent result for this target
+		$run_required = !$this->minion->cache->contains($target);
 
 		$this->indent = str_repeat('  ', ++$this->level);
 		foreach($deps as $dep) {
@@ -42,15 +78,18 @@ class PepperState {
 		}
 		$this->indent = str_repeat('  ', --$this->level);
 
-		// run this target, forcing if any dependants were updated
-		$result = $this->minion->speck($target, $run_required);
+		// run the target if neccesary
+		if($run_required) {
+			$result = $this->minion->speck($target, true);
 
-		$t = $this->minion->task($task);
-		if($t->cache_time($method) == Minion_Cache::CACHE_FLAG) {
-			$run_required = (bool) $result;
+			// allow FLAG methods to cancel the bubble
+			$t = $this->minion->task($task);
+			if($t->cache_time($method) == Minion_Cache::CACHE_FLAG) {
+				$run_required = (bool) $result;
+			}
 		}
 		
-		
+		// report the outcome
 		$action = $run_required ? "REBUILT" : "up to date";
 		$this->minion->log("{$this->indent}- {$target} [{$action}]");
 		
@@ -58,6 +97,11 @@ class PepperState {
 		return $run_required;
 	}
 	
+	/**
+	 * Parser for psudo-yaml makefiles
+	 * @param string $data	raw data
+	 * @throws Exception
+	 */
 	static function parse($data) {
 		$lines = explode("\n", str_replace("\r", "", $data));
 		$result = array();
